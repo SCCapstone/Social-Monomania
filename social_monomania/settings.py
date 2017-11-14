7bdb5de32a68dcbae84f7f0d262af37d3f169cf2
Logan Heckle <loganheckle@gmail.com>
2017-11-14 18:49:58 -0500

    Update settings.py

--- social_monomania/settings.py
+++ social_monomania/settings.py
@@ -124,9 +124,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = (
-	os.path.join(PROJECT_ROOT, 'static'),
-)
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
