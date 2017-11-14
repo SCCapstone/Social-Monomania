5eb08ad39d5b1bf653ab5cf78bea723f5c6cee05
Logan Heckle <loganheckle@gmail.com>
2017-11-14 16:25:58 -0500

    Update settings.py

--- social_monomania/settings.py
+++ social_monomania/settings.py
@@ -116,9 +116,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-	os.path.join(BASE_DIR, 'assets'),
+	os.path.join(PROJECT_ROOT, 'static'),
 )
 
