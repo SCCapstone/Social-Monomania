f8e8e323c46d1ff055dbd15d8752e013193d12eb
lheckle <loganheckle@gmail.com>
2017-11-14 16:00:13 -0500

    blah

--- social_monomania/settings.py
+++ social_monomania/settings.py
@@ -80,6 +80,10 @@ DATABASES = {
     }
 }
 
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
