17b2fb65a8f719671a54e077207fb5cebedcff14
Logan Heckle <loganheckle@gmail.com>
2017-11-14 19:11:39 -0500

    Updated Settings.py one last time
    At this point my fingers have probably fallen off from updating the same file about 20 times.

--- social_monomania/settings.py
+++ social_monomania/settings.py
@@ -121,9 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets"),
+    '/assets/',
+]
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
