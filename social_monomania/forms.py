85f1896d60a20d5f57f6fcfb71b2f75b4f18d965
malBaskin <mebaskin@email.sc.edu>
2017-11-18 19:00:17 -0500

    css

--- social_monomania/forms.py
+++ social_monomania/forms.py
@@ -3,8 +3,8 @@ from django import forms
 class ContactForm(forms.Form):
 
     from_email = forms.EmailField(required=True, widget=forms.TextInput(attrs={
-                    'class': 'email_input'}))
+                    'class': 'email_input', 'placeholder': 'email'}))
     subject = forms.CharField(required=True, widget=forms.TextInput(attrs={
-                    'class': 'subject_input'}))
+                    'class': 'subject_input', 'placeholder': 'subject'}))
     message = forms.CharField(widget=forms.Textarea(attrs={
-                    'class': 'message_input'}))
+                    'class': 'message_input', 'placeholder': 'Your message here...'}))
