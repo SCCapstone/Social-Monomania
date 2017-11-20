e0a952b136e880efd5fc2efbd1e9598494a7635b
malBaskin <mebaskin@email.sc.edu>
2017-11-20 10:53:53 -0500

    css

--- social_monomania/forms.py
+++ social_monomania/forms.py
@@ -4,7 +4,7 @@ class ContactForm(forms.Form):
 
     from_email = forms.EmailField(required=True, label= 'Email', widget=forms.TextInput(attrs={
                     'class': 'email_input', 'placeholder': 'email'}))
-    subject = forms.CharField(required=True, widget=forms.TextInput(attrs={
+    subject = forms.CharField(required=True, margin_top=20px, widget=forms.TextInput(attrs={
                     'class': 'subject_input', 'placeholder': 'subject'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={
+    message = forms.CharField(margin_top=20px,widget=forms.Textarea(attrs={
                     'class': 'message_input', 'placeholder': 'Your message here...'}))
