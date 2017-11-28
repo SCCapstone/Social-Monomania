1f459269b4869bff581d1abc66fde0b16540f02e
malBaskin <mebaskin@email.sc.edu>
2017-11-28 08:49:24 -0500

    contact us backend update

--- social_monomania/settings.py
+++ social_monomania/settings.py
@@ -142,3 +142,13 @@ STATICFILES_DIRS = [
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+
+#contact us backend
+EMAIL_BACKEND =
+  'django.core.mail.backends.console.EmailBackend' 
+DEFAULT_FROM_EMAIL = 'testing@example.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False 
+EMAIL_PORT = 1025
