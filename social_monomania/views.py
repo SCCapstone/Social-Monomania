dbcc1ef0ac99973bd73d15692ea3a7b172398c20
malBaskin <mebaskin@email.sc.edu>
2017-11-30 10:21:38 -0500

    contact us

--- social_monomania/views.py
+++ social_monomania/views.py
@@ -26,7 +26,7 @@ def contact(request):
             from_email = form.cleaned_data['from_email']
             message = form.cleaned_data['message']
             try:
-                send_email(subject, message, from_email, ['socialmonomania@gmail.com'])
+                send_mail(subject, message, from_email, ['socialmonomania@gmail.com'])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('thanks')
