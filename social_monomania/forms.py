4d863147496f9fe0b442ac45d37f3c777ae0f31c
malBaskin <mebaskin@email.sc.edu>
2017-12-01 09:27:42 -0500

    contact us

--- social_monomania/forms.py
+++ social_monomania/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class ContactForm(forms.Form):
 
-    email = forms.EmailField(required=True, label= 'Email', widget=forms.TextInput(attrs={
+    from_email = forms.EmailField(required=True, label= 'Email', widget=forms.TextInput(attrs={
                     'class': 'email_input', 'placeholder': 'Your email here'}))
     subject = forms.CharField(required=True, widget=forms.TextInput(attrs={
                     'class': 'subject_input', 'placeholder': 'Please keep your subject concise...'}))
