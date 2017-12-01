686c0020eb0695515cf5f8355e08e8bce72f2452
Logan Heckle <loganheckle@gmail.com>
2017-11-30 21:10:48 -0500

    Adding admin.py dependencies.

--- online/admin.py
+++ online/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django import forms
+from .models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 
 # Register your models here.
