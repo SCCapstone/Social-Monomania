4c0b7f0608176a2b7eebd7ac567b7c029a0520cc
Sorrento110 <powellfendley@gmail.com>
2017-12-06 18:28:21 -0500

    Facebook API weaned off of facebook python library because it was broken. Also converted to Python 2.

--- utilities/facebookAPI.py
+++ utilities/facebookAPI.py
@@ -1,43 +1,26 @@
 import sys, os
 
-import urllib.request
+import urllib2
 import json
 
-import facebook
 
 def search(args = None):
 
 	if(args == None):
 		args = sys.argv[1:]
-	try:
-		##### Call to get a different kind of access token commented out.  Using a 'User Access Token'
-	# url = "https://graph.facebook.com/oauth/access_token?client_id=154288395206748&client_secret=b31c53b79daff5a17ac4849c839ae998&grant_type=client_credentials"
-	# response = urllib.request.urlopen(url)
-	# tokenJSON = response.readline().decode('utf-8')
-	# token = json.loads(tokenJSON)
-		graph = facebook.GraphAPI(access_token="EAACMUxD9QFwBAIWJ1Oy6aW7cSOb2t4Udyu7rMhS8pQoZARMulQKawhhdzOKDnrj3pXZC0RkBcnNx2InPTcz4I43sIcUs8vUkhCZCJgkoCTaH8mI2yUJknt3kNr09rZBN3bpYXQfjPb0eGHfTWm5zn03UD4zw5GnicUT8CVdRbv1OOkUsmoX9iZBvkbsk3CHDFApWgvbpacAZDZD", 
-		version="2.10")
-	except Exception as e:
-		print(e)
-	else:
-		print("Facebook Graph API initialization Success")
 
+	query = args
+	access_token = "EAACMUxD9QFwBAP3JReCVOxRsSNZAczKtfbvPlFw3UkZARAzeFbbZAF6Bg19jaSHEVLDg6QFmQwZCh2x3KNsPOPgTLnZBbsZC1vQHCblTaBuhJ7WiMhEttCbqd3V65THGb3dXP97KgkK0dWmrssSj0IbXzCZAtxBIpX5K0kQlcZAuHxfWO9e7aNjg3sTDK4AXskE8YiR3OBwFxwZDZD"
 
+	url = "https://graph.facebook.com/search?access_token={0}&q={1}&type=event".format(access_token, query)
+	print(url)
 
-	data = graph.search(type='event', q=args[0])
-	print(data)
+	r = urllib2.urlopen(url)
+	resultJSON = r.readline().decode('utf-8')
+	result = json.loads(resultJSON)
+	
+	result['data'] = [ item for item in result['data']
+                   if query.lower() in item['name'].lower() ]
 
-	results = ''
-	i = 1
-	for d in data:
-		results += "Entry " + str(i) + "--\n"
-		for k in d:
-			try:
-				print(str(d) + ":" + str(k))
-			except Exception as e:
-				print(e)
-			#Once we discover the fields we want to pull on return we can use this loop to append them to a string and display that to the user.
-			results += str(d) + ":" + str(k) + " \n"
-		i += 1
+	print [ item['name'] for item in result['data'] ]
 
-	return data
