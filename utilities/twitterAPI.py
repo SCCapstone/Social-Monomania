e39b428fc120e7e96e9039ec4f38583d5e809aca
Logan Heckle <loganheckle@gmail.com>
2018-03-05 11:08:24 -0500

    Updated Twitter API
    Now outputs tweet info to file, as per needed.

--- utilities/twitterAPI.py
+++ utilities/twitterAPI.py
@@ -5,6 +5,7 @@ import urllib2
 import json
 
 import oauth2
+import unicodedata
 
 
 CONSUMER_KEY = "VFDbTduxt6SeTwyOjOFIfwWIO"
@@ -19,11 +20,14 @@ def search(args):
 	#r = urllib2.urlopen(url)
 	#resultJSON = r.readline().decode('utf-8')
 	resultJSON = oauth_req(url, '3270317358-uXCQfUGY86T1EBPIrGX97s7EkNzzZide84mfgHo' , 'CCdhkak0eOQDxfdAcbdfCkHn91Hdd5SMlldbLtOQFpfPw')
-	result = json.loads(resultJSON)
-
-	print result
-
-	return result
+	result_parsed = json.loads(resultJSON)
+	statuses = result_parsed.get("statuses")
+	f = open('twitter.txt', 'w')
+	for x in statuses:
+		print x.get("user").get("screen_name")
+		f.write(x.get("user").get("screen_name").encode("UTF-8") +": " + x.get("text").encode("UTF-8") +"\n")
+	f.close()
+	return result_parsed
 
 def oauth_req(url, key, secret, http_method="GET", post_body="", http_headers=None):
 
