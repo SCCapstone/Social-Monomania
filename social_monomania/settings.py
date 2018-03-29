fdbe36c4dbfa8ebf8abd2a3a72e34e9e464f8bbb
lheckle <loganheckle@gmail.com>
2018-03-29 13:25:18 -0400

    Testing New Email Backend

--- social_monomania/settings.py
+++ social_monomania/settings.py
@@ -133,6 +133,7 @@ STATICFILES_DIRS = (
 
 #email settings
 EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'socialmonomania@gmail.com'
