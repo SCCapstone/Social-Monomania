832d4cf7815105fe6f5e1de5d1a65cb1341f8dd6
malBaskin <mebaskin@email.sc.edu>
2018-04-19 14:34:21 -0400

    max field lengths
    #104

--- social_monomania/forms.py
+++ social_monomania/forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 class ContactForm(forms.Form):
 
-    from_email = forms.EmailField(required=True, label= 'Email', widget=forms.TextInput(attrs={
-                    'class': 'email_input', 'placeholder': 'Your email here'}))
-    subject = forms.CharField(required=True, widget=forms.TextInput(attrs={
+    from_email = forms.EmailField(required=True, label= 'Email', max_length=20, widget=forms.TextInput(attrs={
+                    'class': 'email_input', 'placeholder': 'Your email here' }))
+    subject = forms.CharField(required=True,max_length=20, widget=forms.TextInput(attrs={
                     'class': 'subject_input', 'placeholder': 'Please keep your subject concise...'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={
+    message = forms.CharField(max_length=500, widget=forms.Textarea(attrs={
                     'class': 'message_input', 'placeholder': 'Your message here...'}))
