2273a44af6df7ff09210d223c1db7a0a36806566
Charles <Charleszengdong@gmail.com>
2018-04-30 07:39:21 -0400

    Update views.py

--- online/views.py
+++ online/views.py
@@ -116,6 +116,6 @@ def change_password(request):
             messages.error(request, 'Please correct the error below.')
     else:
         form = PasswordChangeForm(request.user)
-    return render(request, 'accounts/change_password.html', {
+    return render(request, 'change_password.html', {
         'form': form
     })
