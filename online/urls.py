8ac6072a4e4561e74d6989028374ee09a99a5262
Charles <Charleszengdong@gmail.com>
2018-04-30 20:24:58 -0400

    Update urls.py

--- online/urls.py
+++ online/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include , url
 from online import views
-from django.contrib.auth.views import login, logout, password_reset, password_reset_done
 
 urlpatterns = [
     url(r'^$', views.login, name='login'),
@@ -10,7 +9,7 @@ urlpatterns = [
     url(r'^logout/$', views.logout, name='logout'),
     url(r'^loggedout/$', views.loggedout, name='loggedout'),
     url(r'^registered/$', views.registered, name='registered'),
-    url(r'^reset-password/$', password_reset, name='reset_password'),
-    url(r'^reset-password/done/$', password_reset_done, name='password_reset_done'),
+    url(r'^password-reset/$', views.password_reset, name='reset_password'),
+    url(r'^password-reset/done/$', views.password_reset_done, name='password_reset_done'),
     
 ]
