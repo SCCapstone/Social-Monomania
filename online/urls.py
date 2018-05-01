99b3d0873f085c7ee0f140564518c6814a3b9e0d
Charles <Charleszengdong@gmail.com>
2018-04-30 22:01:51 -0400

    Update urls.py

--- online/urls.py
+++ online/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^loggedout/$', views.loggedout, name='loggedout'),
     url(r'^registered/$', views.registered, name='registered'),
     url(r'^password/$', views.change_password, name='change_password'),
+    url(r'^sendEmail/$',views.sendemail,name='sendemail'),
     
     
     
